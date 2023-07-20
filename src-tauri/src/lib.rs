36b0fc0eb6694fa20dda978cf3e5f1ffb2bbc386
Dima Lipin <dimich3d@ya.ru>
2023-07-20 17:10:01 +0200

    upd store

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,2 +1,14 @@
+use std::path::PathBuf;
+
 pub mod state;
 
+
+pub fn get_store_path() -> PathBuf {
+    let mut path = PathBuf::from(env!("XDG_DATA_HOME"));
+    path.push("yappy");
+    if !path.exists() {
+        std::fs::create_dir_all(&path).unwrap();
+    }
+    path.push("store.json");
+    path
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use app::get_store_path;
 use async_std::task;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
+use tauri::Wry;
+use tauri::async_runtime::handle;
+use tauri_plugin_store::StoreCollection;
 use std::{thread, time::Duration};
-// use tauri
 use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu};
+use tauri_plugin_store::with_store;
+use tauri_plugin_store::StoreBuilder;
 
 use std::sync::{Arc, Mutex};
 use tauri::Manager;
@@ -25,9 +31,13 @@ impl Default for AppState {
 async fn countdown(handle: tauri::AppHandle, state: State<'_, Arc<Mutex<AppState>>>) {
     handle.emit_to("main", "pomo_started", "").unwrap();
     state.lock().unwrap().kill_switch = false;
-    for i in (1..=5).rev() {
+    let stores = handle.state::<StoreCollection<Wry>>();
+    let duration = with_store(handle.clone(), stores, get_store_path(), |store| {
+        let dura = store.get("duration").expect("duration exist").clone();
+        Ok(dura)
+    }).unwrap();
+    for i in (1..=duration.as_i64().expect("is int")).rev() {
         if state.lock().unwrap().kill_switch {
-            state.lock().unwrap().kill_switch = false;
             return;
         }
         println!("Time remaining: {} seconds", i);
@@ -65,6 +75,7 @@ fn main() {
     let system_tray = SystemTray::new().with_menu(tray_menu);
     match tauri::Builder::default()
         .system_tray(system_tray)
+        .plugin(tauri_plugin_store::Builder::default().build())
         .manage(state)
         .setup(|app| {
             let main_window = app.get_window("main").unwrap();
@@ -75,8 +86,13 @@ fn main() {
     {
         Ok(app) => {
             println!("Started");
-            app.emit_to("main", "start", "Booyaka").unwrap();
-            println!("emited");
+            let mut store = StoreBuilder::new(app.handle(), get_store_path()).build();
+            println!("Store: {:?}", store);
+            if !store.has("duration") {
+                println!("No duration");
+                store.insert("duration".to_string(), json!(300)).unwrap();
+                store.save().unwrap();
+            }
             app.run(|_app, event| match event {
                 tauri::RunEvent::ExitRequested { api, .. } => {
                     api.prevent_exit();
